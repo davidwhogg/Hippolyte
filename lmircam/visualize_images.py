861e0d02c85d3cfee6a3befb8b467c67c6e8bdbc
David W. Hogg <dwhogg@gmail.com>
2012-07-30 21:51:22 +0000

    better photometry; there is a trend with phase

--- lmircam/visualize_images.py
+++ lmircam/visualize_images.py
@@ -28,26 +28,28 @@ def estimate_sigma(scene, nsigma=3.5, tol=0.0):
         ms_old = ms
     return np.sqrt(ms)
 
-def hogg_imshow(ax, image, sigma, foo):
+def hogg_imshow(ax, image, mu, sigma):
     ax.set_yticks([])
     ax.set_xticks([])
     ax.set_frame_on(False)
     #    asinh = lambda img, mu, sigma, f: f * np.arcsinh((img - mu) / sigma) + 0.2
     #    plotimage = asinh(image[64-32:64+32,64-32:64+32], np.median(image), sigma, foo)
-    plotimage = (image[64-32:64+32,64-32:64+32] - np.median(image)) / sigma
+    plotimage = (image - mu) / sigma
     return ax.imshow(plotimage, vmin=-40., vmax=200.0, interpolation="nearest")
 
 def hogg_savefig(fig, fn):
     print "saving {0}".format(fn)
     return fig.savefig(fn)
 
-def get_amplitude_and_phase(image, k = 1.1525):
+def get_amplitude_and_phase(image, sky, k = 1.2):
     ygrid, xgrid = np.mgrid[0:image.shape[0], 0:image.shape[1]]
     cosimage = np.cos(k * xgrid)
     sinimage = np.sin(k * xgrid)
+    cosimage /= np.sqrt(np.sum(cosimage * cosimage))
+    sinimage /= np.sqrt(np.sum(sinimage * sinimage))
     A = np.sum(image * cosimage)
     B = np.sum(image * sinimage)
-    return np.sqrt(A ** 2 + B ** 2), np.arctan2(B, A)
+    return np.sum(image - sky), np.arctan2(B, A)
 
 def main():
     file_pattern = "/data2/dfm/lucky/skemer/*.fits"
@@ -55,34 +57,32 @@ def main():
     files = [os.path.abspath(e) for e in sorted(entries)]
     assert len(files) > 0, \
         "There are no files matching '{0}'".format(file_pattern)
+    hw = 8
     nn = 6
     plt.gray()
     figsize = (10,10)
     fig1 = plt.figure(figsize=figsize)
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(1, 1, 1)
+    amps = np.zeros(len(files))
+    phis = np.zeros(len(files))
     for i,fn in enumerate(files):
         hdulist = pyfits.open(fn)
         image = hdulist[0].data
         hdulist.close()
-        print i, fn, image.shape
+        print i, fn, image.shape, np.min(image), np.max(image)
+        sky = np.median(image)
+        a, p = get_amplitude_and_phase(image[65-hw:65+hw,65-hw:65+hw], sky)
         if i == 0:
             sigma = estimate_sigma(image)
         if i < (nn * nn):
             ax1 = fig1.add_subplot(nn, nn, (i % (nn * nn)) + 1)
-            hogg_imshow(ax1, image, sigma, 0.1)
+            hogg_imshow(ax1, image[65-hw:65+hw,65-hw:65+hw], sky, sigma)
         if (i + 1) == (nn * nn):
             hogg_savefig(fig1, "{0}.png".format(os.path.basename(fn)))
-        amp, phase = get_amplitude_and_phase(image)
-        if amp < 60000:
-            print fn
-            for k in 0.01 * np.arange(150):
-                a, p = get_amplitude_and_phase(image, k=k)
-                print k, a
-            assert False
-        ax2.plot(phase, amp, 'ko', alpha=0.5)
-        if i > 36:
-            break
+        amps[i] = a
+        phis[i] = p
+    fig2 = plt.figure()
+    ax2 = fig2.add_subplot(1, 1, 1)
+    ax2.plot(phis, amps, 'ko', alpha=0.5)
     hogg_savefig(fig2, "amp-phase.png")
     return None
 
