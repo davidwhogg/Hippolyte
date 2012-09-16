db0f911067e9e50b62a646f13cce705c5678f137
David W. Hogg <dwhogg@gmail.com>
2012-09-16 00:51:58 +0000

    added coefficient plotting; fergus was right, I am wrong

--- lmircam/data_driven_model.py
+++ lmircam/data_driven_model.py
@@ -15,6 +15,7 @@ import pyfits
 import numpy as np
 import scipy.linalg as la
 import pylab as plt
+import triangle as tri
 
 def read_data(files):
     """
@@ -28,11 +29,14 @@ def read_data(files):
         hdulist.close()
         if images is None:
             images = np.zeros((len(files), image.shape[0], image.shape[1]))
-            print images.shape
         images[i,:,:] = image
     return images
 
-def pca(images, K, plotname=None):
+def hogg_savefig(fn):
+    print "hogg_savefig: Writing %s" % fn
+    return plt.savefig(fn)
+
+def pca(images, K, plotname=None, usvh=None):
     """
     Run PCA on a three-dimensional `(N, nx, ny)` data block `images`,
     return the `(nx, ny)` mean image, the `(K, nx, ny)` eigenimages,
@@ -42,7 +46,9 @@ def pca(images, K, plotname=None):
     N, nx, ny = images.shape
     meanimage = np.mean(images, axis=0)
     datamatrix = (images - meanimage[None, :, :]).reshape(N, nx * ny)
-    u, s, vh = la.svd(datamatrix, full_matrices=False)
+    if usvh is None:
+        usvh = la.svd(datamatrix, full_matrices=False)
+    u, s, vh = usvh
     # check component ordering
     assert np.all(s[:-1] - s[1:] >= 0.)
     if plotname is not None:
@@ -51,12 +57,12 @@ def pca(images, K, plotname=None):
         plt.xlabel('eigenimage')
         plt.ylabel('variance contribution')
         plt.loglog()
-        plt.savefig(plotname)
+        hogg_savefig(plotname)
     eigenimages = vh[:K, :].reshape(K, nx, ny)
     # not completely sure of the "transpose" of the following line
     coefficients = u[:, :K] * s[None, :K]
     reconstructs = np.dot(coefficients, vh[:K, :]).reshape(N, nx, ny) + meanimage[None, :, :]
-    return meanimage, eigenimages, coefficients, reconstructs
+    return meanimage, eigenimages, coefficients, reconstructs, usvh
 
 def main():
     """
@@ -68,23 +74,32 @@ def main():
     assert len(files) > 0, \
         "There are no files matching '{0}'".format(file_pattern)
     images = read_data(files)
-    print "main: Starting PCA..."
-    meanimage, eigenimages, coefficients, recons = pca(images, 16, plotname="pca.png")
-    print "main: ...done PCA"
-    print meanimage.shape, eigenimages.shape, coefficients.shape, recons.shape
-    v1, v2 = np.percentile(images, [0.01, 99])
-    for j in range(len(files)):
-        plt.figure(figsize=(8,4))
-        plt.gray()
+    jplotlist = np.round(len(files) * np.random.uniform(size=5)).astype(int)
+    usvh = None
+    pn = "pca.png"
+    for K in 2 ** np.arange(6):
+        print "main: Starting PCA for K = %d ..." % K
+        meanimage, eigenimages, coefficients, recons, usvh = pca(images, K, plotname=pn, usvh=usvh)
+        pn = None
+        print "main: ...Done PCA for K = %d" % K
+        v1, v2 = np.percentile(images, [0.01, 99])
+        for j in jplotlist:
+            plt.figure(figsize=(8,4))
+            plt.gray()
+            plt.clf()
+            plt.subplots_adjust(left=0.05, right=0.95, wspace=0.05)
+            plt.subplot(1,2,1)
+            imj = images[j, 32:32+64, 32:32+64]
+            rej = recons[j, 32:32+64, 32:32+64]
+            plt.imshow(imj, vmin=v1, vmax=v2, interpolation="nearest")
+            plt.subplot(1,2,2)
+            plt.imshow(rej, vmin=v1, vmax=v2, interpolation="nearest")
+            hogg_savefig("recon-%03d-%02d.png" % (j, K))
+        plt.figure(figsize=(8,8))
         plt.clf()
-        plt.subplots_adjust(left=0.05, right=0.95, wspace=0.05)
-        plt.subplot(1,2,1)
-        imj = images[j, 32:32+64, 32:32+64]
-        rej = recons[j, 32:32+64, 32:32+64]
-        plt.imshow(imj, vmin=v1, vmax=v2, interpolation="nearest")
-        plt.subplot(1,2,2)
-        plt.imshow(rej, vmin=v1, vmax=v2, interpolation="nearest")
-        plt.savefig("recon-%03d.png" % j)
+        labels = ["coefficient $A_{%02d}$" % k for k in range(K)]
+        tri.corner(coefficients.transpose(), labels=labels)
+        plt.savefig("coeffs-%02d.png" % (K))
     return None
 
 if __name__ == '__main__':
